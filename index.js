296425d0214b8ae5d91caefbdb6c4b0cfd71adbe
KarenHippitaku <hippitaku@gmail.com>
2021-02-28 10:01:40 -0600

    First constants and function for file reading, added to index.js . Still not resolved.

---
+++ index.js
@@ -0,0 +1,29 @@
+//Welcome to the module
+module.exports = (givenPath, ids) => { //With this line, you can export the whole thing from the beginning
+	'use strict';
+	return new Promise ((resolve) => {
+		const fs = require('fs'); //The file system module
+		const path = require('path'); //The path module provides utilities for working with file and directory paths *Windows-style paths* :(
+		const rl = require('readline'); //To read line by line
+		const cwd = process.cwd(); //Returns the directory of the process, Current Working Directory
+		
+		const readFile = (givenPath) => {
+			return new Promise ((resolve, reject) => {
+				fs.readFile(givenPath, 'utf8', (err, file) => {
+					if (err) {
+						reject(err);
+						console.error('¡Algo salió mal! ¿Seguro que era ahí? (⊙﹏⊙)');
+					} else {
+						let readableData = data.toString(); //without this line, we'll get buffer data with numbers
+						resolve(file);
+						console.log(readableData);
+					}
+// 					return err ? reject(err) : resolve(file); //It can also responde this way, but I want to use the if else for now
+				})
+			})
+// 			const getOperations = (file) => {
+// 				
+// 			}
+		}
+	})
+}
